bff1cd61bcd52917207e5075373ef6db8ba7fec5
Admin <mybook.link.2025@gmail.com>
2025-10-03 00:02:34 -0500

    Actualizar backend mock con registro y login funcional

--- mybook-backend/models/User.js
+++ mybook-backend/models/User.js
@@ -1,9 +1,2 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email:    { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-});
-
-export default mongoose.model("User", userSchema);
+// Este archivo es solo para que la importación no falle
+export default {};
